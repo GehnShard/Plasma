e59a40784b4a710f248fba98b278c1841f19ecbe
branan <branan@gmail.com>
2012-02-06 22:53:39 -0800

    Bump buildID

--- Sources/Plasma/NucleusLib/pnProduct/Private/pnPrBuildId.cpp
+++ Sources/Plasma/NucleusLib/pnProduct/Private/pnPrBuildId.cpp
@@ -57,7 +57,7 @@ You can contact Cyan Worlds, Inc. by email legal@cyan.com
 
 // This line must NEVER be modified manually; it is automatically updated
 // by the build server.
-#define BUILD_ID 1
+#define BUILD_ID 2
 COMPILER_ASSERT(BUILD_ID != 0);
 
 
