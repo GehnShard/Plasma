8d3af2fefe24c6ecc42efbac779dfa76da0c3318
Adam Johnson <AdamJohnso@gmail.com>
2013-05-04 19:54:19 -0400

    How did I miss this?

--- Python/nb01EmgrPhase0.py
+++ Python/nb01EmgrPhase0.py
@@ -184,7 +184,7 @@ class nb01EmgrPhase0(ptResponder):
                     self.IManageLinkPedestal(position)
 
             # Hard-coded hack: turn off GZ glass just in case...
-            ageSDL["nb01StainedGlassGZ"] = (0,)
+            self._UpdateVaultSDL("nb01StainedGlassGZ", 0)
 
         # Age State Randomization
         # This used to be in the Nexus, but that would never affect server-generated hoods
