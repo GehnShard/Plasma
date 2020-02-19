a458dbaa803aba64d1177e2463af35ba837b17e0
Patrick Dulebohn <Ptrck10381@gmail.com>
2020-02-19 14:36:26 -0500

    Add Chiso Preniv Age Files
    Add the age, fni, py and sdl files for Chiso Preniv.

---
+++ Python/ChisoPreniv.py
@@ -0,0 +1,9 @@
+from Plasma import *
+from PlasmaTypes import *
+
+class ChisoPreniv(ptResponder):
+
+    def __init__(self):
+        ptResponder.__init__(self)
+        self.id = -1
+        self.version = 1
